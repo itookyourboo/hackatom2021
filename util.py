f49629fcce10044b09301182461963414394868b
itookyourboo <Wignorbo@yandex.ru>
2021-02-05 16:21:50 +0300

    web-app

--- util.py
+++ util.py
@@ -1,13 +1,26 @@
 from time import time
 from pydub import AudioSegment
+import os
 
 
 DEBUG = False
+
+LANGUAGES = [
+    ('en-US', 'English (US)'),
+    ('en-UK', 'English (UK)'),
+    ('ru-RU', 'Русский (RU)')
+]
+
 LANGUAGE_EN = 'en-US'
 LANGUAGE_RU = 'ru-RU'
 BUCKET_NAME = 'korzina'
 SAMPLE_RATE = 8000
 RECOGNITION_TIMEOUT = 120
+MAX_AUDIO_SIZE_MB = 32
+APP_NAME = 'hackatom2021'
+UPLOAD_FOLDER = 'audio'
+ALLOWED_AUDIO_EXTENSIONS = {'wav', 'mp3'}
+SECRET_KEY = 'hackatom2021_vihuhol'
 
 
 def time_left(from_time):
---
+++ web_app.py
@@ -0,0 +1,64 @@
+import os
+
+from util import MAX_AUDIO_SIZE_MB, LANGUAGES, \
+    ALLOWED_AUDIO_EXTENSIONS, UPLOAD_FOLDER, APP_NAME, SECRET_KEY
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileRequired, FileField
+from wtforms import SubmitField, SelectField
+from flask_bootstrap import Bootstrap
+
+from google_cloud_stt import transcribe_file
+from analyzer import GoogleCloudResultsAnalyzer as Analyzer
+
+
+app = Flask(__name__)
+app.config['MAX_FILE_CONTENT'] = MAX_AUDIO_SIZE_MB * 1024 * 1024
+app.config['SECRET_KEY'] = SECRET_KEY
+bootstrap = Bootstrap(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    upload_form = AddAudioForm()
+    upload_form.language.choices = LANGUAGES
+
+    if upload_form.validate_on_submit():
+        file_name = upload_form.file.data.filename
+        if '.' not in file_name:
+            upload_form.submit.errors.append('Файл не имеет расширения')
+        else:
+            file_ext = file_name.rsplit('.', 1)[1].lower()
+            if file_ext not in ALLOWED_AUDIO_EXTENSIONS:
+                upload_form.submit.errors.append(f'Неподдерживаемое расширение')
+            else:
+                file = request.files['file']
+                file.save(os.path.join(UPLOAD_FOLDER, file_name))
+
+                return redirect(url_for('analyze', filename=file_name, language=upload_form.language.data))
+
+    return render_template('index.html', title=APP_NAME, upload_form=upload_form)
+
+
+@app.route('/analyze', methods=['GET', 'POST'])
+def analyze(filename='audio.mp3', language='en-US'):
+    results = transcribe_file(os.path.join(UPLOAD_FOLDER, filename), language)
+    analyzer = Analyzer(results)
+
+    return analyzer.get_text()
+
+
+class AddAudioForm(FlaskForm):
+    language = SelectField('Выберите язык', coerce=str)
+
+    file = FileField("Выберите аудио", validators=[
+        FileRequired('Загрузите аудиофайл'),
+        FileAllowed(list(ALLOWED_AUDIO_EXTENSIONS), "Неподдерживаемое расширение")
+    ])
+
+    submit = SubmitField("Распознать текст")
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1')
